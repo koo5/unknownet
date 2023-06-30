a5b67bd705ffb0102107937ad86aae70349d3eea
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-30 02:03:06 +0200

    ..

--- eee/Program.cs
+++ eee/Program.cs
@@ -13,7 +13,7 @@ namespace eee
             Console.WriteLine("Hello World!");
 
             WordprocessingDocument doc =
-                    WordprocessingDocument.Open("doc.docx", true);
+                    WordprocessingDocument.Open("data/doc.docx", true);
 
             /*DocumentSettingsPart settingsPart = doc.MainDocumentPart.GetPartsOfType<DocumentSettingsPart>().First();
             // Create object to update fields on open
@@ -24,6 +24,30 @@ namespace eee
             settingsPart.Settings.Save();*/
 
 
+            /*Console.WriteLine("BookmarkStart:");
+            foreach (var bs in doc.MainDocumentPart.RootElement.Descendants<BookmarkStart>())
+            {
+                Console.WriteLine(">");
+                Console.WriteLine(bs.OuterXml);
+
+            }
+            Console.WriteLine("BookmarkEnd:");
+            foreach (var bs in doc.MainDocumentPart.RootElement.Descendants<BookmarkEnd>())
+            {
+                Console.WriteLine(">");
+                Console.WriteLine(bs.OuterXml);
+
+            }*/
+
+            //Console.WriteLine("fields:");
+            foreach (FieldCode field in doc.MainDocumentPart.RootElement.Descendants<FieldCode>())
+            {
+                //Console.WriteLine(">");
+                //Console.WriteLine(field.Text);
+                field.Text = "";
+            }
+
+            Console.WriteLine("paragraphs:");
             var paragraphs = doc.MainDocumentPart.Document.Body.OfType<Paragraph>().ToList();
             foreach (var p in paragraphs)
             {
@@ -32,19 +56,36 @@ namespace eee
                     p.ParagraphProperties.ParagraphStyleId.Val.Value.Contains("Heading1"))
                 {
 
-                    Console.Out.WriteLine(p.ToString());
+                    //Console.Out.WriteLine(p.ToString());
 
-                    var fields = p.Descendants<FieldChar>();
+                    /*var fields = p.Descendants<FieldChar>();
 
                     foreach (var field in fields)
                     {
                         Console.WriteLine("Hi!");
                         Console.WriteLine(field.InnerXml);
                     }
+                    */
 
+                    //Console.Out.WriteLine(p.InnerText);
+                    String t = "";
+                    foreach (var ch in p.Descendants())
+                    {
+                        if (ch.InnerText != "")
+                        {
+                            if (ch is Text)
+                                t += ch.InnerText;
+                            /*foreach (var a in ch.GetAttributes())
+                            {
+                                Console.Out.WriteLine(a.LocalName + "=" + a.Value);
+                            }
+                            */
+                        }
+                        if (ch is TabChar)
+                            t += "<TAB>";
+                    }
 
-                    Console.Out.WriteLine(p.InnerText);
-                    Console.Out.WriteLine(p.InnerXml);
+                    Console.Out.WriteLine(t);
 
                 }
             }
